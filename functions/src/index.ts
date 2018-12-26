532dee05f9e913a771f31e1a5496cc231bccca91
maechabin <mitoppo@gmail.com>
2018-12-26 21:59:36 +0900

    cors対応

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,6 @@
 import * as functions from 'firebase-functions';
+import * as Cors from 'cors';
+const cors = Cors();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
@@ -9,6 +11,8 @@ admin.initializeApp();
 export const helloWorld: functions.HttpsFunction = functions.https.onRequest(
   (request: functions.Request, response: functions.Response): void => {
     const name = (request.query && request.query.name) || 'world';
-    response.send(`Hello ${name} from Firebase!`);
+    cors(request, response, () => {
+      response.status(200).send({ text: `Hello ${name} from Firebase!` });
+    });
   },
 );
